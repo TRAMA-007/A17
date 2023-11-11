837049e1150d197ad7adc223804ef9fa543e7bec
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2023-11-11 14:55:05 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -763,6 +763,7 @@ Typed *surrender* to surrender and admited defeat`
       'good afternoon': `Good afternoon to you too ${pushname} ✨. Wishing you an enjoyable afternoon too 😇🤞🏻.`,
       'good night': `Good night to you too ${pushname} 😇. Sleep well and sweet dreams.`,
       'كسمك': `ربنا يسامحك`,
+      'ليج': `اوووفووو امشي استحمى يا خول ريحتك واصلاني هنا🤮`,
     };
 
     const smallinput = budy.toLowerCase();
