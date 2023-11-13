2995ce7c2d36f9198dbb6ba5587a9522ff3ac92f
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2023-11-13 22:37:01 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -5416,6 +5416,313 @@ _Click the button below to download_`
         break;
 
 
+    const YT = require("../lib/Ytdl-Core.js");
+const fs = require("fs");
+const yts = require("youtube-yts");
+const ffmpeg = require("fluent-ffmpeg");
+const {
+  fetchJson,
+  getBuffer,
+  GIFBufferToVideoBuffer,
+} = require("../lib/Function2.js");
+
+let mergedCommands = [
+  "play",
+  "song",
+  "ytmp3",
+  "mp3",
+  "ytaudio",
+  "yta",
+  "ytmp4",
+  "mp4",
+  "ytvideo",
+  "ytv",
+  "video",
+];
+
+module.exports = {
+  name: "mediaDownloader",
+  alias: [...mergedCommands],
+  uniquecommands: ["song", "video", "ytmp3", "ytmp4"],
+  description: "All file dowloader commands",
+  start: async (A17, m, { inputCMD, text, doReact, prefix, pushName }) => {
+    switch (inputCMD) {
+      case "play":
+      case "song":
+        if (!text) {
+          await doReact("❌");
+          return m.reply(
+            `Please provide a song name !\n\nExample: *${prefix}song despacito*`
+          );
+        }
+        await doReact("📥");
+        thumbA17 = "https://cdn.donmai.us/sample/b2/77/__sensei_and_plana_blue_archive_drawn_by_fpanda__sample-b277ea432a04ccedd806a4d64bcb329e.jpg";
+        songInfo = await yts(text);
+        song = songInfo.videos[0];
+        videoUrl = song.url;
+        videoId = videoUrl.split("v=")[1];
+
+        await Arashi.sendMessage(
+          m.from,
+          {
+            image: { url: song.thumbnail },
+            caption: `\nDownloading: *${song.title}*
+            
+_🕛 Duration:_ *${song.timestamp}*
+
+_🎀 Channel Name:_ *${song.author.name}*
+
+_🏮 Video Uploaded:_ *${song.ago}*\n`,
+          },
+          { quoted: m }
+        );
+
+        YT.mp3(videoId).then((file) => {
+          const inputPath = file.path;
+          const outputPath = inputPath + ".opus";
+
+          ffmpeg(inputPath)
+            .format("opus")
+            .on("error", (err) => {
+              console.error("Error converting to opus:", err);
+            })
+            .on("end", async () => {
+              //const thumbnailBuffer = await getBuffer(song.thumbnail);
+              const thumbnailBuffer = await getBuffer(thumbArashi);
+
+              await A17.sendPresenceUpdate("recording", m.from);
+
+              Arashi.sendMessage(
+                m.from,
+                {
+                  audio: fs.readFileSync(outputPath),
+                  mimetype: "audio/mpeg",
+                  ptt: true,
+                  /*contextInfo: {
+                    externalAdReply: {
+                      title: song.title.substr(0, 50),
+                      body: `Downloaded by: ${botName}`,
+                      thumbnail: thumbnailBuffer,
+                      mediaType: 1,
+                      mediaUrl: thumbAtlas,
+                      sourceUrl: song.url,
+                    },
+                  },*/
+                },
+                { quoted: m }
+              );
+
+              fs.unlinkSync(inputPath);
+              fs.unlinkSync(outputPath);
+            })
+
+            .save(outputPath);
+        });
+
+        break;
+
+      case "ytmp3":
+      case "mp3":
+      case "ytaudio":
+        if (
+          !text ||
+          (!text.includes("youtube.com/watch?v=") &&
+            !text.includes("youtu.be/"))
+        ) {
+          await doReact("❌");
+          return m.reply(
+            `Please provide a valid YouTube Video link to download as audio!\n\nExample: *${prefix}mp3 put_link*`
+          );
+        }
+        await doReact("📥");
+        songInfo = await yts(text);
+        song = songInfo.videos[0];
+        videoUrl = song.url;
+        videoId = videoUrl.split("v=")[1];
+        thumbArashi = "https://cdn.donmai.us/sample/b2/77/__sensei_and_plana_blue_archive_drawn_by_fpanda__sample-b277ea432a04ccedd806a4d64bcb329e.jpg";
+
+        await A17.sendMessage(
+          m.from,
+          {
+            image: { url: song.thumbnail },
+            caption: `\nDownloading: *${song.title}*
+            
+_🕛 Duration:_ *${song.timestamp}*
+
+_🎀 Channel Name:_ *${song.author.name}*
+
+_🏮 Video Uploaded:_ *${song.ago}*\n`,
+          },
+          { quoted: m }
+        );
+
+        YT.mp3(videoId).then((file) => {
+          const inputPath = file.path;
+          const outputPath = inputPath + ".opus";
+
+          ffmpeg(inputPath)
+            .format("opus")
+            .on("error", (err) => {
+              console.error("Error converting to opus:", err);
+            })
+            .on("end", async () => {
+              const thumbnailBuffer = await getBuffer(thumbArashi);
+
+              await Arashi.sendPresenceUpdate("recording", m.from);
+
+              A17.sendMessage(
+                m.from,
+                {
+                  audio: fs.readFileSync(outputPath),
+                  mimetype: "audio/mpeg",
+                  ptt: true,
+                  /*contextInfo: {
+                    externalAdReply: {
+                      title: song.title.substr(0, 50),
+                      body: `Downloaded by: ${botName}`,
+                      thumbnail: thumbnailBuffer,
+                      mediaType: 1,
+                      mediaUrl: thumbArashi,
+                      sourceUrl: song.url,
+                    },
+                  },*/
+                },
+                { quoted: m }
+              );
+
+              fs.unlinkSync(inputPath);
+              fs.unlinkSync(outputPath);
+            })
+
+            .save(outputPath);
+        });
+
+        break;
+
+      case "ytmp4":
+      case "mp4":
+      case "ytvideo":
+        if (
+          !text ||
+          (!text.includes("youtube.com/watch?v=") &&
+            !text.includes("youtu.be/"))
+        ) {
+          await doReact("❌");
+          return m.reply(
+            `Please provide a valid YouTube Video link to download as audio!\n\nExample: *${prefix}mp4 put_link*`
+          );
+        }
+        await doReact("📥");
+        songInfo = await yts(text);
+        song = songInfo.videos[0];
+        videoUrl = song.url;
+        videoId = videoUrl.split("v=")[1];
+        result = await yts(videoId);
+
+        await A17.sendMessage(
+          m.from,
+          {
+            image: { url: song.thumbnail },
+            caption: `\nDownloading: *${song.title}*
+            
+_🕛 Duration:_ *${song.timestamp}*
+
+_🎀 Channel Name:_ *${song.author.name}*
+
+_🏮 Video Uploaded:_ *${song.ago}*\n`,
+          },
+          { quoted: m }
+        );
+
+        const ytaud3 = await YT.mp4(videoUrl);
+        Arashi.sendMessage(
+          m.from,
+          {
+            video: { url: ytaud3.videoUrl },
+            caption: `${song.title} By: *${botName}*`,
+          },
+          { quoted: m }
+        );
+
+        break;
+
+      case "video":
+        if (!text) {
+          await doReact("❌");
+          return m.reply(
+            `Please provide an YouTube video name !\n\nExample: *${prefix}video dandilions*`
+          );
+        }
+        await doReact("📥");
+
+        songInfo = await yts(text);
+        song = songInfo.videos[0];
+        videoUrl = song.url;
+        videoId = videoUrl.split("v=")[1];
+
+        await A17.sendMessage(
+          m.from,
+          {
+            image: { url: song.thumbnail },
+            caption: `\nDownloading: *${song.title}*
+            
+_🕛 Duration:_ *${song.timestamp}*
+
+_🎀 Channel Name:_ *${song.author.name}*
+
+_🏮 Video Uploaded:_ *${song.ago}*\n`,
+          },
+          { quoted: m }
+        );
+
+        const ytaud2 = await YT.mp4(videoUrl);
+        A17.sendMessage(
+          m.from,
+          {
+            video: { url: ytaud2.videoUrl },
+            caption: `${song.title} By: *${botName}*`,
+          },
+          { quoted: m }
+        );
+
+        break;
+
+      case "yts":
+      case "ytsearch":
+        if (!args[0]) {
+          await doReact("❌");
+          return m.reply(`Please provide a search term!`);
+        }
+        await doReact("📥");
+        let search = await yts(text);
+        let thumbnail = search.all[0].thumbnail;
+        let num = 1;
+
+        var txt = `*🏮 YouTube Search Engine 🏮*\n\n_🧩 Search Term:_ *${args.join(
+          " "
+        )}*\n\n*📌 Total Results:* *${search.all.length}*\n`;
+
+        for (let i of search.all) {
+          txt += `\n_Result:_ *${num++}*\n_🎀 Title:_ *${
+            i.title
+          }*\n_🔶 Duration:_ *${i.timestamp}*\n_🔷 Link:_ ${i.url}\n\n`;
+        }
+
+        let buttonMessage = {
+          image: { url: thumbnail },
+          caption: txt,
+        };
+
+        Arashi.sendMessage(m.from, buttonMessage, { quoted: m });
+        break;
+
+      default:
+        break;
+    }
+  },
+};
+
+
 
       //////////////////////////////////////////////////////////////////////////////////////////////////
       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
