534d6b02f3140da4ae8bb1bdaf749dd1828eca5a
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2023-11-14 15:58:01 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -839,20 +839,15 @@ Typed *surrender* to surrender and admited defeat`
     }
 
 
-    if (smallinput.includes('شرموط')) {
-    reply (`الشغل ما عيب`);
-           }
-
-
+    
     if (smallinput.includes('احشك')) {
     reply (`تحشني بي زبك دا🤏😂؟');
     }
 
 
-  /*  if (smallinput.includes('عب')|| smallinput.includes('عبيد')) {
-    reply (`عب الينيكك`);
-    }*/
-
+    if (smallinput.includes('شرموط')) {
+    reply (`الشغل ما عيب`);
+           }
 
 
 
