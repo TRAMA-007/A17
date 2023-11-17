e2d98e66561bf4dab5c53e6d55e61c63c531740b
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2023-11-17 11:54:33 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -413,10 +413,19 @@ module.exports = A17 = async (A17, m, chatUpdate, store) => {
     //Dm and Groups Autoreply/Bot chat
     
     if (!isCmd && !m.isGroup){
-        const botreply = await axios.get(`http://api.brainshop.ai/get?bid=166512&key=5nz1Ha6nS9Zx1MfT&uid=[uid]&msg=[msg]=[${budy}]`)
-        txt = `${botreply.data.cnt}`
-        m.reply(txt)
-        }    
+        try {
+          const apiUrl1 = `https://vihangayt.me/tools/chatgpt3?q=${encodeURIComponent(q)}`;
+
+          const response1 = await fetch(apiUrl1);
+          const responseData1 = await response1.json();
+
+          let message = "";
+
+          if (response1.status === 200 && responseData1 && responseData1.status === true && responseData1.data) {
+            message = responseData1.data;
+          } else {
+            return reply("Sorry, I couldn't fetch a response from the API at the moment.");
+          }
         
      
 
