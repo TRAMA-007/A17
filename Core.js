39d76e990f4817a72a6601e82f1a3cf9468f3730
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-03-26 01:54:29 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -5862,7 +5862,7 @@ break;
 
     const tawfik = await axios.get(`https://api.lolhuman.xyz/api/stickerwa?apikey=GataDios&query=${encodeURIComponent(q)}`);
     reply(mess.waiting);
-    const essam = tawfik.data.result[1].stickers;
+    const essam = tawfik.data.result[0].stickers;
     // قم بنسخ هذا الجزء واستبداله مع الجزء السابق في الكود
 const imageCount = 10; // عدد الصور التي تريد إرسالها
 
