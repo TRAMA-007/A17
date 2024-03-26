14184a300c00bb30ec1d641a65afc5de765ce404
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-03-26 06:49:33 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -8088,14 +8088,13 @@ break;
         A17.sendMessage(from, { react: { text: "🍁", key: m.key } });
         if (!text) return reply(`Please proide a search term!\n\n*Example:* ${prefix}genshin hu tao`)
         
-        const q = await axios.get(`https://api.lolhuman.xyz/api/genshin/hu tao?apikey=GataDios`)
+        const q = await axios.get(`https://api.lolhuman.xyz/api/genshin/${q} ?apikey=GataDios`)
         reply(mess.waiting);
         const w = q.data.result;
         let hutao = `
         *Name*: ${w.title}
         *Description*: ${w.intro} `;
         await A17.sendMessage(m.chat, { image: { url: w.cover1}, caption: hutao }, { quoted: m })
-        await A17.sendMessage(m.chat, { audio: { url: w.cv[0].audio[0] }, { quoted: m })
       }
         break;
 
