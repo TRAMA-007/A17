8fa4b17d964427a4457f677bbdf562069f6e6b4f
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-04-16 21:51:45 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -1142,8 +1142,7 @@ Typed *surrender* to surrender and admited defeat`
 
 
     if (smallinput.includes('كسم بلانا') || smallinput.includes('بلانا شرموط') || smallinput.includes('كسم البوت')) {
-    let users = m.sender
-        await A17.groupParticipantsUpdate(m.chat, users, 'remove')
+     await A17.groupParticipantsUpdate(m.chat, m.sender, 'remove')
     if (isBotAdmins) await A17.sendMessage(from, { text: 'الراجل يرجعه' });
   } 
                                                                                                                
