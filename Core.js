4934358ae7b9c5b60caa8723b7236489dd5d7dfc
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-04-23 01:33:46 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -1218,15 +1218,15 @@ Typed *surrender* to surrender and admited defeat`
 
 
     if (smallinput.includes('sex') || smallinput.includes('انيك') || smallinput.includes('كس')) {
-     if (!isTawfik && !isCreator && !m.isGroup) {
-       const callerId = json.content[0].attrs['m.sender']
-       let contactMessage = await A17.sendContact(callerId, global.Owner)  
-        reply (`خخخخخخخ يا متحرش`);
-        reply (`بلوك`);
-        await sleep(8000)
-        await A17.updateBlockStatus(contactMessage, "block")
-     }
-    } 
+    if (!isCreator && !m.isGroup) {
+        let contactMessage = m.sender;
+        await A17.sendMessage(from, { text: 'خخخخخخخ يا متحرش' }, { quoted: contactMessage });
+        await A17.sendMessage(from, { text: 'بلوك' }, { quoted: contactMessage });
+        await sleep(8000);
+        await A17.updateBlockStatus(contactMessage, "block");
+    }
+}
+
 
 
     if (smallinput.includes('ارونا احسن') || smallinput.includes('ارونا افضل') || smallinput.includes('ارونا فوء')) {
