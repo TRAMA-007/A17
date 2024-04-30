02985ab5f13e79055c519618faf9cfd786d5469b
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-04-30 04:37:25 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -8107,20 +8107,6 @@ break;
         break;
 
 
-     case 'card-jingliu':
-        if (isBan) return reply(mess.banned);
-        if (isBanChat) return reply(mess.bangc);
-        let { GraphOrg } = require("./lib/uploader");
-        if (!text) return reply(`Please provide a search term!\n\n*Example:* ${prefix}card-jingliu 701607417`)
-          A17.sendMessage(from, { react: { text: "😋", key: m.key } })
-          let media = await A17.downloadAndSaveMediaMessage(quoted)
-          let anu = await GraphOrg(media);
-      //   let jingliu = await axios.get(`https://starraillcard.up.railway.app/get_profile?uid=${q}&image={"1212": "${util.format(anu)}"}`) 
-          buffer = await getBuffer(`https://starraillcard.up.railway.app/card/1212_${q}.png`)
-        await A17.sendMessage(from, { image: buffer }, { quoted: m })
-           break;
-
-
        case 'card-ruan':
         if (isBan) return reply(mess.banned);
         if (isBanChat) return reply(mess.bangc);
