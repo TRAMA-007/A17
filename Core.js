46e5f79297946d918f5f0b690d7c71b3f9a39201
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-05-03 19:21:05 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -10638,7 +10638,7 @@ const characterAI = new CharacterAI();
         if (!text) return reply(`Please proide a search term!\n\n*Example:* ${prefix}ba aru`)
         
         const ba = await axios.get(`https://api.atlasacademy.io/nice/JP/servant/search?name=${encodeURIComponent(q)}`)
-        onst aru = ba.data;
+        const aru = ba.data;
         const traitNames = aru.traits.map(traits => traits.name).join(',');
         let arutxt = `
   *Name: ${aru.name}*
