5ec8c52b79d79d2a11cf9dcabf5c703ed3076119
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-05-04 11:36:15 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -500,6 +500,33 @@ module.exports = A17 = async (A17, m, chatUpdate, store) => {
     }
 
 
+    // Assuming 'message' is the object representing the incoming message
+const sxx = message.mimetype || ''; // Get mimetype or set to empty string if undefined
+const shiroko = /image|video|sticker|audio/.test(mimeType);
+let { GraphOrg } = require("./lib/uploader");
+
+if (shiroko && m.isGroup) {
+let media = await A17.downloadAndSaveMediaMessage(quoted)
+        let anu = await GraphOrg(sxx);
+  let serika = await axios.get(`https://nsfw-categorize.it/api/upload?url=${util.format(anu)}`)
+        const sensei = serika.data.data;
+  if (serika.nsfw === true && serika.porn === true) {
+    let { chat, fromMe, id } = m;
+    
+    const key = {
+        remoteJid: chat,
+        fromMe: fromMe,
+        id: id,
+        participant: m.sender
+    };
+        await A17.sendMessage(chat, { delete: key });
+      if (isBotAdmins) await A17.sendMessage(from, { text: '*nudity detected message deleted*' });
+  }
+
+}
+
+
+
 
     //
     // const hariRaya = new Date("6 1, 2022 00:00:00");
