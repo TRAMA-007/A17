3d511d76d4a12dba25e8f7f4137c47203e2208fd
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-06-06 16:42:20 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -933,18 +933,9 @@ Typed *surrender* to surrender and admited defeat`
 
    if (smallinput.includes('شوفونا') || smallinput.includes('شوفنا')) {
   const media = await getBuffer("https://media1.tenor.com/m/L4QUJbE-Zc8AAAAC/cat-cat-side-eye.gif");
-   const stickerBuffer = await toSticker(media, {
-    pack: 'Pack_name', // The pack name
-    author: 'Your_name', // The author name
-       })
-A17.sendMessage(from, { sticker: stickerBuffer }, { quoted: m });
-// Convert to WebP using sharp
- // const webpBuffer = await sharp(media)
- //   .webp()
-//    .toBuffer();
-  // Send sticker using A17 library (replace with your actual function)
- // A17.sendMessage(from, { sticker: webpBuffer, animated: true }, { quoted: m });
-		      }
+   const stickerBuffer = await toSticker(media, { pack: 'Pack_name',author: 'Your_name' })
+    A17.sendMessage(from, { sticker: stickerBuffer }, { quoted: m });
+	      }
 
 
  
