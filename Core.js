641b5bc4a637556f3dd3b0f819540a224c508cb1
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-06-28 11:02:30 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -152,7 +152,7 @@ if (global.db)
 
 
 //
-let isSleeping = false; // Move tthe declaraation here.
+let isSleeping = false; // Move tthe declaraation herge.
 let banUser = JSON.parse(fs.readFileSync('./database/banUser.json'));
 let banchat = JSON.parse(fs.readFileSync('./database/banChat.json'));
 let kaiaudio = JSON.parse(fs.readFileSync('./Media-Database/audio.json'));
