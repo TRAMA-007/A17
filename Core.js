6959fa04c2101747e734959afdd37cdcf9609337
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-07-28 15:21:10 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -1017,7 +1017,7 @@ Typed *surrender* to surrender and admited defeat`
 
    if (smallinput.includes('lop')) {
     const { Image } = require ('react-native-compressor') 
-   const result = await Image.compress('./Assets/pic10.jpg'
+   const result = await Image.compress('./Assets/pic10.jpg') 
     A17.sendMessage(from, { image : result }, { quoted: m });
        }
 
